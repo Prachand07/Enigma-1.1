45a213c7b3de8c11931c94cccc9084f189d5f712
Aarush Luthra <163556661+Prachand07@users.noreply.github.com>
2025-03-19 23:50:50 +0530

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,17 +6,8 @@ sudo systemctl enable httpd
 cd /var/www/html/Frontend
 sudo echo "CONFIG = { PUBLIC_IP: '$(curl -s ifconfig.me)' };" > config.js
 cd /var/www/html/backend
-REPO_FILE="/etc/yum.repos.d/mongodb-org-8.0.repo"
-echo "[mongodb-org-8.0]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/
-gpgcheck=1
-enabled=1
-gpgkey=https://pgp.mongodb.com/server-8.0.asc" | sudo tee $REPO_FILE > /dev/null
-sudo yum install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
-aws ssm get-parameters --names "MONGO_CLUSTER" "MONGO_DB" "MONGO_OPTIONS" "MONGO_PASSWORD" "MONGO_USER" "PORT" --with-decryption --query "Parameters[*].[Name,Value]" --output text | awk '{print $1 "=" $2}' > .env
+wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+export $(aws ssm get-parameters --names "MONGO_URL" "PORT" --with-decryption --query "Parameters[*].[Name,Value]" --output text | awk '{print $1 "=" $2}')
 sudo yum install -y nodejs
 sudo npm install -g pm2
 sudo pm2 start index.js
